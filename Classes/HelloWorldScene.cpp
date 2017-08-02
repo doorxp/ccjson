41d03f012e43f2aae019071dd909a848452753d0
doorxp <doorxp@msn.com>
2017-08-02 09:48:53 +0800

    bkb

--- Classes/HelloWorldScene.cpp
+++ Classes/HelloWorldScene.cpp
@@ -31,12 +31,12 @@ bool HelloWorld::init()
     
     
     const char *jsonString = CCJSONSTR({'a':"b","arr":["中文","日文","English","🤣😂😊😇🙂😍🍇"]});
-    
+
     CCJSONPtr json = CCJSON::parse(jsonString);
     CCJSONPtr arr = json->objectForKey("arr");
-    
+
     CCLOG("%s",json->objectForKey("a")->as_string().c_str());
-    
+
     //list Child node
     JSON_FOREACH(arr, i) {
         CCLOG("%s", i->as_string().c_str());
@@ -44,41 +44,41 @@ bool HelloWorld::init()
     else {
         CCLOG("no data");
     }
-    
-    
+
+
     Data data = FileUtils::getInstance()->getDataFromFile("test.json");
-    
+
     json = CCJSON::parse((const char *)data.getBytes());
-    
+
     CCJSONPtr glossary = json->objectForKey("glossary");
-    
+
     CCJSONPtr title = glossary->objectForKey("title");
-    
+
     CCLOG("%s", title->as_string().c_str());
-    
-    
+
+
     CCJSONPtr glossDiv = glossary->objectForKey("GlossDiv");
-    
+
     //get Multi-level child nodes
     CCJSONPtr abbrev = glossDiv->objectForKeys("GlossList","GlossEntry","Abbrev",NULL);
-    
+
     CCLOG("%s", abbrev->as_string().c_str());
-    
+
     //get Multi-level child nodes with path
     CCJSONPtr glossSeeAlso = glossDiv->objectForPath("GlossList/GlossEntry/GlossDef/GlossSeeAlso");
-    
+
     JSON_FOREACH(glossSeeAlso, i) {
         CCLOG("%s", i->as_string().c_str());
     }
     else {
         CCLOG("no data");
     }
-    
+
     //get Multi-level child nodes with path, Array
     CCJSONPtr XML = glossDiv->objectForPath("GlossList/GlossEntry/GlossDef/GlossSeeAlso/@0");
-    
+
     CCLOG("%s", XML->as_string().c_str());
-    
+
     
     //create json
     
@@ -91,9 +91,9 @@ bool HelloWorld::init()
     ar->push(1);
     ar->push(1.5);
     ar->push(true);
-    
-    node->push(ar);
-    
+
+    node->push("arr1", ar);
+
     CCLOG("%s",node->str().c_str());
     
     
